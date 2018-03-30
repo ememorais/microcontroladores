6c908b8c59145fe8f78269debd071c0ffb021f47
Marcelo Morais <marcelodotmorais@gmail.com>
2018-03-30 14:03:55 -0300

    Criado LCD_PushConfig.

--- LCD/lcd.s
+++ LCD/lcd.s
@@ -16,6 +16,7 @@ DELAY_BIG   EQU 4
         IMPORT  PortM_Output
             
 ;TABELA: Pág 24 - http://moodle.utfpr.edu.br/pluginfile.php/412456/mod_resource/content/1/Datasheet_HD44780.pdf
+
 ;------------LCD_Init------------
 ; Configura o sistema para utilizar o LCD.
 ; Entrada: Nenhum
@@ -24,63 +25,49 @@ DELAY_BIG   EQU 4
 LCD_Init
     PUSH {R0, LR}
     
-    ;Clear display (0 0 0 0 0 0 0 1)
-	MOV R0, #2_00000000             
-	BL  PortM_Output
-	MOV R0, #0x01                   
-	BL  PortK_Output
-    MOV R0, #DELAY_SMALL
-    BL  SysTick_Wait1us
-	MOV R0, #2_00000100             
-	BL  PortM_Output
+    MOV R0, #0x01
+    BL  LCD_PushConfig
 	MOV R0, #DELAY_BIG
     BL  SysTick_Wait1ms   
     
-    ;Entry Mode Set (0 0 0 0 0 1 I/D S)
-	MOV R0, #2_00000000             
-	BL  PortM_Output
-	MOV R0, #2_00000110                   
-	BL  PortK_Output
-	MOV R0, #2_00000100
-	BL  PortM_Output
-	MOV R0, #DELAY_SMALL
-	BL  SysTick_Wait1us
+    ;Entry Mode Set 
+    MOV R0, #2_00000110     ;(0 0 0 0 0 1 I/D S) 
+    BL LCD_PushConfig
     
-    ;Function Set (0 0 1 DL N F - - )
-	MOV R0, #2_00000000             
-	BL  PortM_Output
-	MOV R0, #2_00111000                  
-	BL  PortK_Output
-	MOV R0, #2_00000100             
-	BL  PortM_Output
-	MOV R0, #DELAY_SMALL
-	BL  SysTick_Wait1us
+    ;Function Set 
+    MOV R0, #2_00111000     ;(0 0 1 DL N F - -)
+    BL  LCD_PushConfig
     
-    ;Display on/off control (0 0 0 0 1 D C B)
-	MOV R0, #2_00000000             
-	BL  PortM_Output
-	MOV R0, #2_00001110             
-	BL  PortK_Output
-	MOV R0, #2_00000100             
-	BL  PortM_Output
-	MOV R0, #DELAY_SMALL
-	BL  SysTick_Wait1us
+    ;Display on/off control
+	
+	MOV R0, #2_00001110     ;(0 0 0 0 1 D C B)          
+	BL  LCD_PushConfig
     
-    ;Return home (0 0 0 0 0 0 1 -)
-	MOV R0, #2_00000000
-	BL  PortM_Output
-	MOV R0, #2_00000010
-	BL  PortK_Output
-	MOV R0, #2_00000100
-	BL  PortM_Output
-	MOV R0, #DELAY_BIG
-	BL  SysTick_Wait1ms
-	MOV R0, #2_00000000
-	BL  PortM_Output
+    ;Return home            ;(0 0 0 0 0 0 1 -)
+    MOV R0, #2_00000010
+    BL  LCD_PushConfig
     
     POP {R0, LR}
     BX LR
-    
+
+;------------LCD_PushConfig------------
+; Envia dados no modo config para o controlador do LCD.
+; Entrada: R0 --> Dado a ser enviado
+; Saída: Nenhum
+; Modifica: R0, R1 (temporariamente), R2 (temporariamente)
+LCD_PushConfig
+    PUSH {R1, R2, LR}
+    MOV R2, R0
+	MOV R0, #2_00000000             
+	BL  PortM_Output
+	MOV R0, R2             
+	BL  PortK_Output
+	MOV R0, #2_00000100             
+	BL  PortM_Output
+	MOV R0, #DELAY_SMALL
+	BL  SysTick_Wait1us
+    POP {R1, R2, LR}
+    BX LR
     
     ALIGN                        ;Garante que o fim da seção está alinhada 
     END                          ;Fim do arquivo
